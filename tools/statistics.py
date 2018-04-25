b3ffb1af352e49e9bc08ca60e3c287af1a1e3346
Jackie Leng <Jackie.Leng@nelen-schuurmans.nl>
2018-04-25 14:26:45 +0200

    clarify

--- tools/statistics.py
+++ tools/statistics.py
@@ -463,7 +463,10 @@ class StatisticsTool:
         end_idx = np.array(end_idx)
         log.info('read flowline results and calculate stats')
         if self.ds.has_groundwater:
-            # TODO: make something prettier
+            # in the groundwater version, the node index starts from 1 instead
+            # of 0. The NetcdfDataSourceGroundwater takes this into account,
+            # but if you use the node index to get data from the netCDF on
+            # your own, you need to subtract 1 from the id.
             start_idx -= 1
             end_idx -= 1
 
@@ -506,10 +509,9 @@ class StatisticsTool:
             vmin = np.minimum(vmin, v)
 
             h = ds.get_values_by_timestep_nr('s1', i)
-            h_array = h
 
-            h_start = h_array[start_idx]
-            h_end = h_array[end_idx]
+            h_start = h[start_idx]
+            h_end = h[end_idx]
             try:
                 np.copyto(dh_max, np.maximum(dh_max, np.asarray(np.absolute(h_start - h_end))),
                       where=np.logical_not(np.logical_or(h_start.mask, h_end.mask)))
